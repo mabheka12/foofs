4bd2184a28ba1b846157d9ff379410082bf7436f
bheki daweti <bheki.daweti@gmail.com>
2026-07-14 19:27:35 +0200

    added weather widget

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,52 @@
 // app/robots.ts
-export default function robots() {
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://roofleakrepaird.com'
+
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/api/',
-    },
-    sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/api/',
+          '/admin/',
+          '/_next/',
+          '/_vercel/',
+          '/favicon.ico',
+          '/_next/static/',
+          '/_next/image/',
+          '/static/',
+          '/images/',
+        ],
+        crawlDelay: 5,
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: ['/api/', '/admin/', '/_next/', '/_vercel/'],
+        crawlDelay: 2,
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+        disallow: ['/api/', '/admin/', '/_next/', '/_vercel/'],
+        crawlDelay: 3,
+      },
+      {
+        userAgent: 'AhrefsBot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'SemrushBot',
+        disallow: '/',
+      },
+      {
+        userAgent: 'MJ12bot',
+        disallow: '/',
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
   }
 }
